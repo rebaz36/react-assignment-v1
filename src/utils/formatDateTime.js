a340fa462c1433a62267f6dc28cb2a36a03071d1
rebaz36 <rebaz415@gmail.com>
2023-02-14 13:31:35 +0300

    Fix formatDateTime function

--- src/utils/formatDateTime.js
+++ src/utils/formatDateTime.js
@@ -1,7 +1,12 @@
 const formatDateTime = (timestamp) => {
   const date = new Date(timestamp);
   // FIX: return the local date in the following format 'YYYY-MM-DDTHH:mm:ss'
-
-  return date;
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
 };
 export default formatDateTime;

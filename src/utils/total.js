e416b18a8a8eac5900f06a424f09cbfb49221616
rebaz36 <rebaz415@gmail.com>
2023-02-14 12:58:28 +0300

    Fix total Function

--- src/utils/total.js
+++ src/utils/total.js
@@ -1,3 +1,5 @@
 // FIX: return the summation of the array elements
-const total = (array) => 0;
+const total = (array) => {
+  return array.reduce((acc, curr) => acc + curr, 0);
+};
 export default total;
